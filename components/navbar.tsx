70604356d1b0768c9269c2e5ce1f3aa5220afe6d
Karthik7259 <mohankartik.cs23@rvce.edu.in>
2026-03-07 20:47:12 +0530

    Update navbar.tsx and package-lock.json

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,7 +9,7 @@ const NAV_LINKS = [
   { href: '/', label: 'Home' },
   { href: '/citizen-file', label: 'File Complaint' },
   { href: '/track', label: 'Track Complaint' },
-  { href: '/citizen-login', label: 'Login' },
+
 ]
 
 export function Navbar() {
